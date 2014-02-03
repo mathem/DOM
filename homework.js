da72230c67ddc17e639ac3ed0d4ff8a5b06a202d
mathem <6terranova9@gmail.com>
2014-02-04 01:07:46 +0200

    update

--- homework.js
+++ homework.js
@@ -1,4 +1,5 @@
 	document.addEventListener('click', _onMouseClick, false);
+	
 	/**
 	 * Обработчик клика по ссылке с классом 'popup-link'
 	 * @param {Event} e событие клика
@@ -17,15 +18,14 @@
 	 * @param {HTMLElement} link Ссылка с data-аттрибутами
 	 */
 	function openPopupFromLink(link) {
-		var title = link.dataset.title;
-		var message = link.dataset.message;
 		var linkh = link.href;
-		message = message.replace('%s', linkh);
-		var onOk = function () {
-			return window.location = link.href;
-		}
-		
-		createPopup(title, message, onOk);
+		createPopup(
+			link.dataset.title,
+			link.dataset.message.replace('%s', linkh),
+			function () { 
+				return window.location = linkh;
+			}
+		);
 	}
 
 	/**
@@ -33,28 +33,27 @@
 	 * @param {String} title Заголовок сообщение
 	 * @param {String} message Текст сообщения сообщение
 	 * @param {Function} onOk Обработчик клика по кнопке 'Да'
-	 * @returns {HTMLElement}
 	 */
+	 
 	function createPopup(title, message, onOk) {
-	if ( document.getElementsByClassName('preview').length == 0) {
+	if (document.getElementsByClassName('preview').length == 0) {
+		document.getElementsByClassName('bg')[0].style.display = 'block';
 		var alert_message = document.createElement('div');
-		document.body.style.background = 'rgba(43, 31, 27, .9)';
 		alert_message.className = 'preview';
 		alert_message.innerHTML ='<h2 class="preview_title">' +title + '</h2><p class="preview_message">' + message + '</p><p class="preview_buttons"><input type="submit" name="ok" value="Да"><input type="submit" name="no" value="Нет"></p>';
 		document.body.appendChild(alert_message);
-		var width_block = document.getElementsByClassName('preview')[0].offsetWidth
-		var height_block = document.getElementsByClassName('preview')[0].offsetHeight;
-		var ok_button = document.getElementsByName('ok')[0];
-		var no_button = document.getElementsByName('no')[0];
-		document.getElementsByClassName('preview')[0].style.marginLeft = width_block*(-0.5) + 'px';
-		document.getElementsByClassName('preview')[0].style.marginTop = height_block*(-0.5) + 'px';
-		ok_button.addEventListener('click', onOk, false);
-		no_button.addEventListener('click', function() {
-			document.body.removeChild(alert_message);
-			document.body.style.background = '#ffffff';
-		}, false);
-		return (alert_message);
+	} else {
+		document.getElementsByClassName('preview')[0].children[0].innerHTML = title;
+		document.getElementsByClassName('preview')[0].children[1].innerHTML = message;
+		document.getElementsByClassName('preview')[0].style.display = 'block';
+		document.getElementsByClassName('bg')[0].style.display = 'block';
 	}
 	
-	}
+	document.getElementsByTagName('INPUT')[0].addEventListener('click', onOk, false);
+	document.getElementsByTagName('INPUT')[1].addEventListener('click', function() {
+		document.getElementsByClassName('preview')[0].style.display = 'none';
+		document.getElementsByClassName('bg')[0].style.display = 'none';
+	}, false);
+	
+}
 
